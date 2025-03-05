62bf3b9408d153c96449373d7b75e8c78298c070
iliaIsr <ilil.sbl91@gmail.com>
2025-03-05 15:33:06 +0300

    globalStyle, theme

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import {Contact} from "./layout/sections/contact/Contact.tsx";
 import {Slogan} from "./layout/sections/slogan/Slogan.tsx";
 import {Footer} from "./layout/sections/footer/Footer.tsx";
 
+
 function App() {
 
 
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,14 +1,20 @@
 import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo.tsx";
 import {Menu} from "../../components/menu/Menu.tsx";
+import {Container} from "../../components/Container.ts";
+import {FlexWrapper} from "../../components/FlexWrapper.tsx";
 
 const items=["Home", "Skills", "Works", "Testimony", "Contact"]
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu items={items}/>
+            <Container>
+                <FlexWrapper justify={'space-between'} align={'center'}>
+                    <Logo/>
+                    <Menu items={items}/>
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
